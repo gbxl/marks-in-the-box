951e8af6f456178124a61aa68f61691cef5eb478
Jonathan Perichon <jonathan.perichon@gmail.com>
2012-09-12 19:31:44 -0400

    load dropbox.js locally

--- background.js
+++ background.js
@@ -1,15 +1,16 @@
-//Load dropbox js at launch
-chrome.tabs.executeScript(null,
-                              {file:"http://cdnjs.cloudflare.com/ajax/libs/dropbox.js/0.6.0/dropbox.min.js"});
+(function() {
+	
 
-//Start loggin procedure
-var client = new Dropbox.Client({
-    key: "d8idp2skkk9yri8", secret: "gyf9wz31f4e7y3x", sandbox: true
-});
 
 //Bookmarks stuff
 chrome.bookmarks.onCreated.addListener(testPrint);
 function testPrint(){
+	//Start loggin procedure
+	var client = new Dropbox.Client({
+	    key: "d8idp2skkk9yri8", secret: "gyf9wz31f4e7y3x", sandbox: true
+	});
+	client.authDriver(new Dropbox.Drivers.Redirect());
+	
     var concatLinks = '';
 
     chrome.bookmarks.getTree( function(bookmarks){listBookmarks(bookmarks[0])})
@@ -21,7 +22,9 @@ function testPrint(){
             }
             else {
                 concatLinks =concatLinks.concat('||', bookmark.url);
-                console.log(concatLinks);
+                //console.log(concatLinks);
             }
         }
 }
+
+})();
