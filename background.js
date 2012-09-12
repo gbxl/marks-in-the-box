84f50c35d3819bc4ef5a1bb144d1b9a877318bf9
Jonathan Perichon <jonathan.perichon@gmail.com>
2012-09-12 19:56:49 -0400

    refactoring

--- background.js
+++ background.js
@@ -1,29 +1,32 @@
 (function() {
 
-    //Bookmarks stuff
-    chrome.bookmarks.onCreated.addListener(testPrint);
-    function testPrint(){
-        var concatLinks = '';
-
-        chrome.bookmarks.getTree(function(bookmarks){listBookmarks(bookmarks[0])});
-        function listBookmarks(bookmark) {
-            if (bookmark.children) {
-                for (var i = 0; i < bookmark.children.length; i++) {
-                    listBookmarks(bookmark.children[i]);
-                }
-            }
-            else {
-                concatLinks =concatLinks.concat('||', bookmark.url);
-                //console.log(concatLinks);
-            }
-        }
-    }
-    chrome.windows.onCreated.addListener(function(Window window) {
-        //Start loggin procedure
-        var client = new Dropbox.Client({
-            key: "d8idp2skkk9yri8", secret: "gyf9wz31f4e7y3x", sandbox: true
-        });
-        client.authDriver(new Dropbox.Drivers.Redirect());
-    });
+	//Bookmarks stuff
+	chrome.bookmarks.onCreated.addListener(function() {
+		chrome.bookmarks.getTree(function(bookmarks){
+			listBookmarks(bookmarks[0]);
+		});
+	});
+	
+	function listBookmarks(bookmark) {
+		var concatLinks = '';
+		if (bookmark.children) {
+			for (var i = 0; i < bookmark.children.length; i++) {
+				listBookmarks(bookmark.children[i]);
+			}
+		}
+		else {
+			concatLinks =concatLinks.concat('||', bookmark.url);
+			//console.log(concatLinks);
+		}
+	};
+	
+	chrome.windows.onCreated.addListener(function() {
+		//Start loggin procedure
+		var client = new Dropbox.Client({
+			key: "d8idp2skkk9yri8", secret: "gyf9wz31f4e7y3x", sandbox: true
+		});
+		console.log("Dropbox Client created");
+		client.authDriver(new Dropbox.Drivers.Redirect());
+	});
 
 })();
