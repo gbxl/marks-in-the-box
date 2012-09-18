ce2a2a5b3b4aa0538296c03207f94bd28ded3765
Jonathan Perichon <jonathan.perichon@gmail.com>
2012-09-18 00:31:24 -0400

    fix indentation

--- background.js
+++ background.js
@@ -1,43 +1,43 @@
 (function() {
-	chrome.tabs.create({ url: "options.html" });
-	
-	var saveToDropbox = function() {
+    chrome.tabs.create({ url: "options.html" });
+
+    var saveToDropbox = function() {
 
         if (client.dropboxUid == null) {
             return;
         }
 
-		chrome.bookmarks.getTree(function(bookmarks){
-			var t = "";
-			var content = listBookmarks(bookmarks[0], t);
-		    client.authDriver = new Dropbox.Drivers.Redirect({ rememberUser: true });
-			client.authenticate(function() {
-				client.writeFile("bookmarks.txt", content, function(error, stat) {
-					if (error) {
-						console.log("error=" + error);
-						return;
-					}
-				});
-			});
-		});
-	};
-	
-	var listBookmarks = function(bookmark, concatLinks) {
-		if (bookmark.children) {
-			for (var i = 0; i < bookmark.children.length; i++) {
-				concatLinks += listBookmarks(bookmark.children[i]);
-			}
-		}
-		else {
-			concatLinks += bookmark.url + '\n';
-		}
-		return concatLinks;
-	};
+        chrome.bookmarks.getTree(function(bookmarks){
+            var t = "";
+            var content = listBookmarks(bookmarks[0], t);
+            client.authDriver = new Dropbox.Drivers.Redirect({ rememberUser: true });
+            client.authenticate(function() {
+                client.writeFile("bookmarks.txt", content, function(error, stat) {
+                    if (error) {
+                        console.log("error=" + error);
+                        return;
+                    }
+                });
+            });
+        });
+    };
 
-	chrome.bookmarks.onCreated.addListener(saveToDropbox);
-	chrome.bookmarks.onRemoved.addListener(saveToDropbox);
-	chrome.bookmarks.onChanged.addListener(saveToDropbox);
-	chrome.bookmarks.onMoved.addListener(saveToDropbox);
-	chrome.bookmarks.onChildrenReordered.addListener(saveToDropbox);
-	chrome.bookmarks.onImportEnded.addListener(saveToDropbox);
+    var listBookmarks = function(bookmark, concatLinks) {
+        if (bookmark.children) {
+            for (var i = 0; i < bookmark.children.length; i++) {
+                concatLinks += listBookmarks(bookmark.children[i]);
+            }
+        }
+        else {
+            concatLinks += bookmark.url + '\n';
+        }
+        return concatLinks;
+    };
+
+    chrome.bookmarks.onCreated.addListener(saveToDropbox);
+    chrome.bookmarks.onRemoved.addListener(saveToDropbox);
+    chrome.bookmarks.onChanged.addListener(saveToDropbox);
+    chrome.bookmarks.onMoved.addListener(saveToDropbox);
+    chrome.bookmarks.onChildrenReordered.addListener(saveToDropbox);
+    chrome.bookmarks.onImportEnded.addListener(saveToDropbox);
 })();
